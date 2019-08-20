4a7a0efe5d870ce91551fc1ffe16e64dc585be7b
Namithajacob <namithajacob147@gmail.com>
2019-08-20 23:31:07 +0100

    minor change in indentation in text_filter.cpp

--- src/text_filter.cpp
+++ src/text_filter.cpp
@@ -135,13 +135,17 @@ void output_filter(const char *input,const char *output){
 			while(for_port != NULL){
 				if(strstr(for_port,"output")){
 					strcpy(port,"output");
-				}else if(strstr(for_port,"data_")){
+				}
+				else if(strstr(for_port,"data_")){
 					strcpy(port,"data_out");
-				}else if(strstr(for_port,"packet_")){
+				}
+				else if(strstr(for_port,"packet_")){
 					strcpy(port,"packet_sent_out");
-				}else if(strstr(for_port,"ack_")){
+				}
+				else if(strstr(for_port,"ack_")){
 					strcpy(port,"ack_received_out");
-				}else if(strstr(for_port,"out")){
+				}
+				else if(strstr(for_port,"out")){
 					strcpy(port,"out");
 				}
 				/**<finds the occurrence of { in the line to find the port value*/
